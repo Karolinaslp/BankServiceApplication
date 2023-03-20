f3e59d3854ef6ba9afa59367adb0224b9ae10cc7
Karolinaslp <slempkarolina@gmail.com>
2023-03-20 15:29:41 +0100

    Add sql query for findAllEager()

--- src/main/java/org/karolina/client/db/ClientJpaRepository.java
+++ src/main/java/org/karolina/client/db/ClientJpaRepository.java
@@ -4,16 +4,18 @@ import org.karolina.client.domain.Client;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ClientJpaRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByEmailToLowerCase(String email);
+    Optional<Client> findByEmailIgnoreCase(String email);
 
     List<Client> findByNameIgnoreCase(String name);
 
-    List<Client> getAllClients();
+    @Query("SELECT DISTINCT c FROM Client c JOIN FETCH c.accounts")
+    List<Client> findAllEager();
 
     Page<Client> findByNameIgnoreCase(String name, Pageable pageable);
 
