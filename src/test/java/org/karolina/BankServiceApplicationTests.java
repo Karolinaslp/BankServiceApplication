fca922a505b2d2d2ea83f2e76ffe75036ce9fb40
Karolinaslp <slempkarolina@gmail.com>
2023-03-20 15:29:01 +0100

    Test updateClient() from ClientService

---
+++ src/test/java/org/karolina/BankServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.karolina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class BankServiceApplicationTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
--- src/test/java/org/karolina/client/application/ClientServiceTest.java
+++ src/test/java/org/karolina/client/application/ClientServiceTest.java
@@ -2,7 +2,10 @@ package org.karolina.client.application;
 
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.karolina.account.db.AccountJpaRepository;
 import org.karolina.account.domain.Account;
+import org.karolina.client.application.port.ClientServiceUseCase.UpdateClientCommand;
+import org.karolina.client.application.port.ClientServiceUseCase.UpdateClientResponse;
 import org.karolina.client.db.ClientJpaRepository;
 import org.karolina.client.domain.Client;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -30,7 +34,10 @@ public class ClientServiceTest {
     ClientService service;
 
     @MockBean
-    ClientJpaRepository repository;
+    ClientJpaRepository clientRepository;
+
+    @MockBean
+    AccountJpaRepository accountRepository;
 
 
     /**
@@ -40,13 +47,13 @@ public class ClientServiceTest {
     public void save_twoDifferentClientWithTheSameMail_secondClientNotAdded() {
         //Given
         String email = "a@a";
-        Client client = new Client("Bartek", email, Set.of(account(BigDecimal.valueOf(100),"PLN")));
+        Client client = new Client("Bartek", email, Set.of(account(BigDecimal.valueOf(100), "PLN")));
         Client client1 = alek(email);
-        when(repository.findByEmailToLowerCase(client.getEmail())).thenReturn(Optional.of(client));
+        when(clientRepository.findByEmailIgnoreCase(client.getEmail())).thenReturn(Optional.of(client));
         //When
         service.save(client1);
         //Then
-        assertFalse(repository.getAllClients().contains(client1));
+        assertFalse(clientRepository.findAllEager().contains(client1));
     }
 
     @SneakyThrows
@@ -54,26 +61,26 @@ public class ClientServiceTest {
     public void save_newClient_Ok() {
         //Given
         Client client = alek("a@a.pl");
-        when(repository.findByEmailToLowerCase(client.getEmail())).thenReturn(Optional.of(client));
+        when(clientRepository.findByEmailIgnoreCase(client.getEmail())).thenReturn(Optional.of(client));
         //When
         service.save(client);
         //Then
-        verify(repository).save(client);
+        verify(clientRepository).save(client);
     }
 
     /**
      * Ignore case when saving the client
      */
     @Test
-    public void save_clientWithUpperCaseEmailOk_clientSaved()  {
+    public void save_clientWithUpperCaseEmailOk_clientSaved() {
         //Given
         String email = "UPPER@eMail.PL";
         Client client = alek(email);
-        when(repository.findByEmailToLowerCase(email)).thenReturn(Optional.of(client));
+        when(clientRepository.findByEmailIgnoreCase(email)).thenReturn(Optional.of(client));
         //When
         service.save(client);
         //Then
-        verify(repository).save(client);
+        verify(clientRepository).save(client);
     }
 
     /**
@@ -85,11 +92,11 @@ public class ClientServiceTest {
         String email = "alek@pl";
         Client client = alek(email);
         Client client2 = alek(email);
-        when(repository.findByEmailToLowerCase(client.getEmail())).thenReturn(Optional.of(client));
+        when(clientRepository.findByEmailIgnoreCase(client.getEmail())).thenReturn(Optional.of(client));
         //When
         service.save(client2);
         //Then
-        assertFalse(repository.getAllClients().contains(client2));
+        assertFalse(clientRepository.findAllEager().contains(client2));
     }
 
     /**
@@ -103,7 +110,7 @@ public class ClientServiceTest {
         //When
         service.save(client);
         //Then
-        assertFalse(repository.getAllClients().contains(client));
+        assertFalse(clientRepository.findAllEager().contains(client));
     }
 
     /**
@@ -117,7 +124,7 @@ public class ClientServiceTest {
         //When
         service.save(client);
         //Then
-        assertFalse(repository.getAllClients().contains(client));
+        assertFalse(clientRepository.findAllEager().contains(client));
     }
 
     /**
@@ -130,7 +137,7 @@ public class ClientServiceTest {
         //When
         service.save(client);
         //Then
-        assertFalse(repository.getAllClients().contains(client));
+        assertFalse(clientRepository.findAllEager().contains(client));
     }
 
     /**
@@ -141,7 +148,7 @@ public class ClientServiceTest {
         //Given
         String email = "alek@pl";
         Client client = alek(email);
-        when(repository.findByEmailToLowerCase(email)).thenReturn(Optional.of(client));
+        when(clientRepository.findByEmailIgnoreCase(email)).thenReturn(Optional.of(client));
         //When
         service.save(client);
         Optional<Client> actualClient = service.findByEmail("ALEK@PL");
@@ -149,14 +156,75 @@ public class ClientServiceTest {
         assertEquals(Optional.of(client), actualClient);
     }
 
+    @Test
+    public void updateClient_updatingClientName_Ok() {
+        //Given
+        String newName = "Olek";
+        Set<Long> accountIds = new HashSet<>(Collections.singletonList(1L));
+
+        Client existingClient = alek("alek@a.pl");
+        when(clientRepository.findById(existingClient.getId())).thenReturn(Optional.of(existingClient));
+
+        Account account = new Account(new BigDecimal("100"), "PLN");
+        when(accountRepository.findById(1L)).thenReturn(Optional.of(account));
+
+        UpdateClientCommand updateClientCommand = new UpdateClientCommand(existingClient.getId(), newName, existingClient.getEmail(), accountIds);
+        //When
+        UpdateClientResponse clientResponse = service.updateClient(updateClientCommand);
+
+        //Then
+        assertTrue(clientResponse.isSuccess());
+        assertEquals(newName, existingClient.getName());
+    }
+
+    @Test
+    void updateClient_shouldAddAccountsToClient_Ok322222() {
+        // given
+        String newName = "New Name";
+        String newEmail = "newemail@example.com";
+        Set<Long> accountIds = new HashSet<>(Collections.singletonList(1L));
+
+        Client existingClient = alek("a@a.pl");
+        when(clientRepository.findById(existingClient.getId())).thenReturn(Optional.of(existingClient));
+
+        Account account = new Account(new BigDecimal("100"), "PLN");
+        when(accountRepository.findById(1L)).thenReturn(Optional.of(account));
+
+        UpdateClientCommand updateClientCommand = new UpdateClientCommand(existingClient.getId(), newName, newEmail, accountIds);
+
+        // when
+        UpdateClientResponse updateClientResponse = service.updateClient(updateClientCommand);
+
+        // then
+        assertTrue(updateClientResponse.isSuccess());
+        assertEquals(newName, existingClient.getName());
+        assertEquals(newEmail, existingClient.getEmail());
+        assertEquals(1, existingClient.getAccounts().size());
+        assertTrue(existingClient.getAccounts().contains(account));
+    }
+    @Test
+    public void updateClient_clientNotFound_falseResponse() {
+        //Given
+        Set<Long> accountsIds = new HashSet<>(Collections.singletonList(1L));
+        UpdateClientCommand command = new UpdateClientCommand(1L,"Alek", "a@a.pl",accountsIds);
+        when(clientRepository.findById(command.getId())).thenReturn(Optional.empty());
+
+        //When
+        UpdateClientResponse response = service.updateClient(command);
+
+        //Then
+        assertThat(response.isSuccess()).isFalse();
+        assertThat(response.getErrors().contains("Client not found with id: 1"));
+    }
+
     Set<Account> accounts = new HashSet<>();
 
     private Client alek(String email) {
         accounts.add(account(BigDecimal.valueOf(100), "PLN"));
-       return new Client("Alek", email, accounts);
+        return new Client("Alek", email, accounts);
     }
 
-    private Account account(BigDecimal balance, String currency ){
+    private Account account(BigDecimal balance, String currency) {
         return Account.builder().balance(balance).currency(currency).build();
     }
 }
